2a60dd5fb96d740d9750ed892fd8c2a8526b9b99
ahmed-khelouf <ahmed.khelouf@clsi.fr>
2022-06-17 15:21:22 +0200

    la boulette

--- src/src/constants.ts
+++
@@ -1,8 +0,0 @@
-import path from 'path'
-
-export const PATH = {
-    USER_SAVE_PATH: path.join(__dirname, '../data/users.json'),
-    MEAL_SAVE_PATH: path.join(__dirname, '../data/meals.json'),
-    MEALSBYUSERS_SAVE_PATH: path.join(__dirname, '../data/MealsByUsers.json'),
-    MEALSBYWEEK_SAVE_FOLDER_PATH: path.join(__dirname, '../data/meals/'),
-}
--- src/src/models/WeekMenu.ts
+++
@@ -1,6 +0,0 @@
-import { Meal } from './Meal'
-
-export interface WeekMenu {
-    date: string
-    meals: Meal[]
-}
